fccfcc608b17cb17a8b65f8aff7daf1abe9d3092
Craig Lockwood <craig@1ockwood.dev>
2023-07-31 21:14:16 -0400

    add site option to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import 'dotenv/config';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.SITE_URL,
   scopedStyleStrategy: 'class',
 });
